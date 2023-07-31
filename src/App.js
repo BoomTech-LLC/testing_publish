fc9184602ff940ac33b0e5e2361404a0f6b093c1
Tigran0199 <cruelman11@gmail.com>
2023-07-31 14:02:01 +0400

    builder init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Builder } from "boomform-builder";
+import styles from "./styles.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper} style={{"--cssVariable":"300px"}}>
+      <Builder
+        global={{
+          name: "Fill your name",
+          onSubmit: (args) => {
+            console.log("Testing onSubmit", args);
+          },
+        }}
+        fields={[
+          {
+            id: "name",
+            type: "name",
+            label: "Please Type Your Name",
+            middleName: true,
+            validations: {
+              HTMLValidate: true,
+              first: { required: { msg: "First name is required" } },
+              last: { required: { msg: "Last name is required" } },
+            },
+          },
+          {
+            id: "scale_rating",
+            type: "scaleRating",
+            min: 0,
+            max: 11,
+            initial: 4,
+          },
+        ]}
+        button={{
+          text: "Submit",
+        }}
+      />
     </div>
   );
 }
